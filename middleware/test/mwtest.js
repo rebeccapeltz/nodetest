6fc330c95a4e89d2b5c1c95672172f1d46ccfeb9
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-05-23 21:28:23 -0700

    add test

---
+++ middleware/test/mwtest.js
@@ -0,0 +1,18 @@
+//http://willi.am/node-mocha-supertest/
+var assert = require("assert");
+var request = require("request");
+
+require("../server");
+
+describe("Server", function() {
+    it("responds with JSON message {\"msg\": \"No model\"} at the root", function(done) {
+        request("http://localhost:3000/api", function(err, response, body) {
+            if (err) done(err);
+
+            var payload = JSON.parse(body);
+            assert.equal(payload.msg, "No model");
+
+            done();
+        });
+    });
+});
